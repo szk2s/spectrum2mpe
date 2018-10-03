5200f9212aae2b0a9faec78fe8da2606c2c48b0b
Yuichi Yogo <yuichkun@gmail.com>
2018-10-04 01:55:58 +0900

    refactor main code

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const Patcher = require('./lib/Patcher');
 // set up the convert options 
 const config = require('./config');
-
+const BOXES = ['CSV Import', 'Analyser', 'SMF Generator', 'SMF Player', 'SMF Export'];
 const patcher = new Patcher(config);
 
-patcher.addBox('CSV Import');
-patcher.addBox('Analyser');
-patcher.addBox('SMF Generator');
-patcher.addBox('SMF Player');
-patcher.addBox('SMF Export');
-
+patcher.initWith(BOXES);
 patcher.run();
