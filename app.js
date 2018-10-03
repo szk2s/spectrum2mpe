dd75dcff8b8c278312a6d70665d9841f8bbe2a5c
Yuichi Yogo <yuichkun@gmail.com>
2018-10-04 02:32:38 +0900

    replace boxlist with BOX_TYPES

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const Patcher = require('./lib/Patcher');
 // set up the convert options 
 const config = require('./config');
-const BOXES = ['CSV Import', 'Analyser', 'SMF Generator', 'SMF Player', 'SMF Export'];
+const { CSV_IMPORT, ANALYSER, SMF_GENERATOR, SMF_PLAYER, SMF_EXPORT } = require('./constants/BOX_TYPES');
+const BOXES = [CSV_IMPORT, ANALYSER, SMF_GENERATOR, SMF_PLAYER, SMF_EXPORT];
 const patcher = new Patcher(config);
 
 patcher.initWith(BOXES);
---
+++ constants/BOX_TYPES.js
@@ -0,0 +1,7 @@
+module.exports = {
+  CSV_IMPORT    : 'CsvImport',
+  ANALYSER      : 'Analyser',
+  SMF_GENERATOR : 'SmfGenerator',
+  SMF_PLAYER    : 'SmfPlayer',
+  SMF_EXPORT    : 'SmfExport'
+}
