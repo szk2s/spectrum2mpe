02aa4f369ee568ac35c38f5f808ebdef087cb67f
Yuichi Yogo <yuichkun@gmail.com>
2018-10-03 19:44:17 +0900

    separate config from app

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 const Patcher = require('./lib/Patcher');
-
 // set up the convert options 
-const config = {
-    inputFreqsFilepath : './assets/test/cello_freqs_16ch.csv',
-    inputAmpsFilepath : './assets/test/cello_amps_16ch.csv',
-    outputFileName : 'cello_test.mid',
-    outputDeviceName : 'Apple DLS Synth',
-    ppqr : 48,   // ticks per quarter note
-    bpm : 120,
-    defaultVelocity : 10,
-    pitchBendRange : 24
-};
+const config = require('./config');
 
 const patcher = new Patcher(config);
 
