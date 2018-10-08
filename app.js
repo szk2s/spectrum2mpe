1aa5cdfcb1df96d85eba6749119a24aa87b3aaa0
szk2s <suzukisjss@gmail.com>
2018-10-08 23:46:38 +0900

    fixed bug

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Patcher = require('./lib/Patcher');
 const config = require('./config');
 
 const testBoxes = ['CsvImport', 'Analyser', 'SmfGenerator', 'SmfPlayer', 'SmfExport'];  //Convert CSV into MPE
+// const testBoxes = ['CsvImport', 'OscSender']    //Send freqs and amps to Reaktor Synth
 // const testBoxes = ['SineGenerator', 'CCtoSMF', 'SMFExport']; //under development
 
 const patcher = new Patcher(config);
--- config.js
+++ config.js
@@ -1,11 +1,26 @@
 module.exports = {
-    inputFreqsFilepath : './assets/user/child_freqs_15ch.csv',
-    inputAmpsFilepath : './assets/user/child_amps_15ch.csv',
-    outputFilepath : './output/childvoice_test.mid',
-    // outputDeviceName : 'Apple DLS Synth',
+    inputFreqsFilepath : './assets/user/csv/bird_freqs.csv',
+    inputAmpsFilepath : './assets/user/csv/bird_amps.csv',
+    inputTimecodeFilepath : './assets/user/csv/bird_timecode.csv',
+    outputFilepath : './output/bird.mid',
     outputDeviceName : 'IAC Driver Bus 1',
     ppqr : 96,   // ticks per quarter note
     bpm : 120,
     defaultVelocity : 10,
-    pitchBendRange : 24
+    pitchBendRange : 24,
+    oscClient : {ip : '127.0.0.1', port : 1234}
 };
+
+
+// default setting
+
+// module.exports = {
+//     inputFreqsFilepath : './assets/test/csv/cello_freqs.csv',
+//     inputAmpsFilepath : './assets/test/csv/cello_amps.csv',
+//     outputFilepath : './output/childvoice_test.mid',
+//     outputDeviceName : 'Apple DLS Synth',
+//     ppqr : 96,   // ticks per quarter note
+//     bpm : 120,
+//     defaultVelocity : 10,
+//     pitchBendRange : 24
+// };
