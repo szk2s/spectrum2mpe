4ead51129432e5603bbb7a3d16ea9039a41e3a93
szk2s <suzukisjss@gmail.com>
2018-10-13 22:22:27 +0900

    start dev SlideCC Box

--- config.js
+++ config.js
@@ -1,26 +1,14 @@
 module.exports = {
-    inputFreqsFilepath : './assets/user/csv/bird_freqs.csv',
-    inputAmpsFilepath : './assets/user/csv/bird_amps.csv',
-    inputTimecodeFilepath : './assets/user/csv/bird_timecode.csv',
-    outputFilepath : './output/bird.mid',
+    inputFreqsFilepath : './assets/user/csv/church2_freqs.csv',
+    inputAmpsFilepath : './assets/user/csv/church2_amps.csv',
+    inputTimecodeFilepath : './assets/user/csv/church2_timecode.csv',
+    inputEnvFilepath:  './assets/user/csv/child_env.csv',
+    outputFilepath : './output/church2.mid',
     outputDeviceName : 'IAC Driver Bus 1',
     ppqr : 96,   // ticks per quarter note
     bpm : 120,
     defaultVelocity : 10,
-    pitchBendRange : 24,
-    oscClient : {ip : '127.0.0.1', port : 1234}
+    pitchBendRange : 48,
+    oscClient : {ip : '127.0.0.1', port : 1234},
+    slideCC : 74
 };
-
-
-// default setting
-
-// module.exports = {
-//     inputFreqsFilepath : './assets/test/csv/cello_freqs.csv',
-//     inputAmpsFilepath : './assets/test/csv/cello_amps.csv',
-//     outputFilepath : './output/childvoice_test.mid',
-//     outputDeviceName : 'Apple DLS Synth',
-//     ppqr : 96,   // ticks per quarter note
-//     bpm : 120,
-//     defaultVelocity : 10,
-//     pitchBendRange : 24
-// };
