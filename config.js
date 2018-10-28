45b3d79477071c35fa9facdc125937eacef83505
szk2s <suzukisjss@gmail.com>
2018-10-28 23:54:25 +0900

    adds assets for test run

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ module.exports = {
     inputFreqsFilepath : './assets/test/csv/bird_freqs.csv',
     inputAmpsFilepath : './assets/test/csv/bird_amps.csv',
     inputTimecodeFilepath : './assets/test/csv/bird_timecode.csv',
-    inputEnvFilepath:  './assets/test/csv/child_env.csv',
-    outputFilepath : './output/church2.mid',
+    inputEnvFilepath:  './assets/test/csv/bird_env.csv',
+    outputFilepath : './output/bird.mid',
     outputDeviceName : 'IAC Driver Bus 1',
-    ppqr : 96,   // ticks per quarter note
+    ppqr : 48,   // ticks per quarter note
     bpm : 120,
     defaultVelocity : 10,
     pitchBendRange : 48,
