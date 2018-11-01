72c1c8da84bd6f2ea9446c853258fc5d7dd4ed7d
szk2s <suzukisjss@gmail.com>
2018-11-01 22:27:54 +0900

    solves minor problem

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const Patcher = require('./lib/Patcher');
 const config = require('./config');
 const BOXES = require('./lib/boxes');
-const testBoxes = [BOXES.CsvImport, BOXES.Analyser, BOXES.SmfGenerator, BOXES.MasterChannelCC, BOXES.SmfPlayer, BOXES.SmfExport];
-// const testBoxes = ['CsvImport', 'OscSender']    //Send freqs and amps to Reaktor Synth
-// const testBoxes = ['SineGenerator', 'CCtoSMF', 'SMFExport']; //under development
+// const testBoxes = [BOXES.CsvImport, BOXES.Analyser, BOXES.SmfGenerator, BOXES.MasterChannelCC, BOXES.SmfPlayer, BOXES.SmfExport];
+const testBoxes = [BOXES.CsvImport, BOXES.OscSender]    //Send freqs and amps to Reaktor Synth
+// const testBoxes = [BOXES.SineGenerator, BOXES.CCtoSMF, BOXES.SMFExport]; //under development
 
 const patcher = new Patcher(config);
 
