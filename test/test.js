480b3f443bfd215dd444550ab78ac35552092693
szk2s <suzukisjss@gmail.com>
2018-11-25 15:03:45 +0900

    replaces some deprecated methods

--- test/test.js
+++ test/test.js
@@ -3,17 +3,26 @@ const S2M = require('..');
 const environmentConfig = require('./config');
 
 const s2m = S2M.build(environmentConfig);
+// s2m.convertFromMatlab('bird');
 s2m.convertFromSpear('large_bowl');
 
 // describe('Build s2m', function() {
 //   it(
 //       'should return type object', 
 //       function(){          
-//         s2m.build(environmentConfig);
 //         assert.typeOf(s2m.config,'object');
 //         assert.property(s2m.config, 'bpm');
 //       }
 //   );
+// });
+
+// describe('Convert from Spear', function() {
+//     it(
+//         'should return file type binary', 
+//         function(){
+//         s2m.convertFromSpear('large_bowl');
+//         }
+//     );
 // });
 
 // describe('Convert from Matlab', function() {
