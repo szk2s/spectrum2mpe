39492c7bfd032bae0522a7707ca40bfc765ff4d3
szk2s <suzukisjss@gmail.com>
2018-12-26 21:12:30 +0900

    improves speed

--- examples/convert.js
+++ examples/convert.js
@@ -1,7 +1,7 @@
 const s2m = require('..');
 const moment = require('moment');
 
-async function convert(){
+const convert = async () => {
     const partials = await s2m.txtImport(__dirname + '/assets/txt/large_bowl.txt');
     const melodies = await s2m.partials2melodies(partials);
     const smfs = await s2m.genSMFs(melodies, 'test-song');
