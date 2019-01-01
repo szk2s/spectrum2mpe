586ac0eb2108f6124758568c2deb05a84d04287d
szk2s <suzukisjss@gmail.com>
2019-01-01 19:55:20 +0900

    Apply code format
    with Eslint and prettier

--- examples/convert_long_text.js
+++ examples/convert_long_text.js
@@ -2,19 +2,14 @@ const s2m = require('..');
 const moment = require('moment');
 
 const convert = async () => {
-    const partials = await s2m.txtImport(__dirname + '/assets/txt/long_text.txt');
-    const melodies = await s2m.partials2melodies(partials);
-    const smfs = await s2m.genSMFs(melodies, 'test-song');
-    await s2m.smfsBatchExport(
-        smfs, 
-        'long_text', 
-        __dirname + '/output', 
-        {
-            makeOutputFolder: true,
-            outputFolderName: moment(new Date()).format('YYMMDD')
-        }
-    );
-    console.log('Completed!');
+  const partials = await s2m.txtImport(__dirname + '/assets/txt/long_text.txt');
+  const melodies = await s2m.partials2melodies(partials);
+  const smfs = await s2m.genSMFs(melodies, 'test-song');
+  await s2m.smfsBatchExport(smfs, 'long_text', __dirname + '/output', {
+    makeOutputFolder: true,
+    outputFolderName: moment(new Date()).format('YYMMDD')
+  });
+  console.log('Completed!');
 };
 
 convert();
