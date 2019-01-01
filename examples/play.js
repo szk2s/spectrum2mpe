eca40102151d597fbf3c77579d52dc004e151e66
szk2s <suzukisjss@gmail.com>
2019-01-02 01:40:25 +0900

    Add example of playing mpe

---
+++ examples/play.js
@@ -0,0 +1,18 @@
+const s2m = require('..');
+
+const play = async () => {
+  const partials = await s2m.txtImport(
+    __dirname + '/assets/txt/large_bowl.txt'
+  );
+  const melodies = await s2m.partials2melodies(partials);
+  const smfs = await s2m.genSMFs(melodies, 'test-song');
+  const ports = await s2m.outputPorts();
+  console.log('Available ports are');
+  ports.forEach((port, idx) => {
+    console.log(idx + ': ' + port.name);
+  });
+  await s2m.smfsMultiPlay(smfs, [1, 2, 3]);
+  console.log('Completed!');
+};
+
+play();
