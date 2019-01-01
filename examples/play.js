4691a207043b737695e24b1756c6be3804ff3d19
szk2s <suzukisjss@gmail.com>
2019-01-01 19:55:20 +0900

    Apply code format
    with Eslint and prettier

--- examples/play.js
+++ examples/play.js
@@ -1,17 +1,17 @@
 const s2m = require('..');
 const moment = require('moment');
 
-const convert = async () => {
+const play = async () => {
   const partials = await s2m.txtImport(
     __dirname + '/assets/txt/large_bowl.txt'
   );
   const melodies = await s2m.partials2melodies(partials);
   const smfs = await s2m.genSMFs(melodies, 'test-song');
-  await s2m.smfsBatchExport(smfs, 'large_bowl', __dirname + '/output', {
+  await s2m.smfsBatchPlay(smfs, 'large_bowl', __dirname + '/output', {
     makeOutputFolder: true,
     outputFolderName: moment(new Date()).format('YYMMDD')
   });
   console.log('Completed!');
 };
 
-convert();
+play();
