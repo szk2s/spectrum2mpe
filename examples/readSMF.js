c0572c19586547480efce7507b1c9cc29955820b
szk2s <suzukisjss@gmail.com>
2019-01-07 17:03:50 +0900

    Integrate readSMF examples
    - You can import multiple SMFs by indicating directory.

--- examples/readSMF.js
+++ examples/readSMF.js
@@ -2,14 +2,14 @@ const s2m = require('..');
 
 const readAndPlaySMF = async () => {
   const smfs = await s2m.smfImport(__dirname + '/assets/mid/cricket.mid');
-
   const ports = await s2m.outputPorts();
   console.log('Available ports are');
   ports.forEach((port, idx) => {
     console.log(idx + ': ' + port.name);
   });
-
-  await s2m.smfPlay(smfs[0], 1);
+  const numTracks = smfs.reduce((acc, smf) => acc + smf.length, 0);
+  const playoutPortIndices = new Array(numTracks).fill().map((_, i) => i + 1);
+  await s2m.smfsMultiPlay(smfs, playoutPortIndices);
 };
 
 readAndPlaySMF();
