d7caa1bd3f73c14e14024f285451709678e0a1d7
szk2s <suzukisjss@gmail.com>
2019-01-27 20:07:29 +0900

    Remove unneeded Promise wrapper

--- src/convert.js
+++ src/convert.js
@@ -5,32 +5,30 @@ import partials2melodies from './partials2melodies';
 import genSMFs from './genSMFs';
 import { smfsBatchExport } from './export';
 
-const convertFromSpear = (
+const convertFromSpear = async (
   inputFilePath,
   _option = {
     outputDirName: path.dirname(inputFilePath),
     makeOutputFolder: true,
     outputFolderName: 'output'
   }
-) =>
-  new Promise(async (resolve) => {
-    const defaultOption = {
-      outputDirName: path.dirname(inputFilePath),
-      makeOutputFolder: true,
-      outputFolderName: 'output'
-    };
-    const { outputDirName, makeOutputFolder, outputFolderName } = _.merge(defaultOption, _option);
+) => {
+  const defaultOption = {
+    outputDirName: path.dirname(inputFilePath),
+    makeOutputFolder: true,
+    outputFolderName: 'output'
+  };
+  const { outputDirName, makeOutputFolder, outputFolderName } = _.merge(defaultOption, _option);
 
-    const songName = path.basename(inputFilePath, '.txt');
-    const partials = await txtImport(inputFilePath);
-    const melodies = await partials2melodies(partials);
-    const smfs = await genSMFs(melodies, songName);
-    await smfsBatchExport(smfs, songName, outputDirName, {
-      makeOutputFolder: makeOutputFolder,
-      outputFolderName: outputFolderName
-    });
-    console.log('Completed!');
-    resolve();
+  const songName = path.basename(inputFilePath, '.txt');
+  const partials = await txtImport(inputFilePath);
+  const melodies = await partials2melodies(partials);
+  const smfs = await genSMFs(melodies, songName);
+  await smfsBatchExport(smfs, songName, outputDirName, {
+    makeOutputFolder: makeOutputFolder,
+    outputFolderName: outputFolderName
   });
+  console.log('Completed!');
+};
 
 export default convertFromSpear;
